7c2af8151c04f56068a71599af44fa4ed1836d7b
Rahul Kumar Sharma <rkumarsharma845@gmail.com>
2022-09-14 14:36:46 +0530

    added multiplication file

---
+++ src/test/java/gitPractice/Multiply.java
@@ -0,0 +1,12 @@
+package gitPractice;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=4,b=5;
+		
+		System.out.println("Multiplication is: "+a*b);
+	}
+
+}
