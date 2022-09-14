cb2681fae3ac8ab685e92498e52e10777b2dd7ef
Rahul Kumar Sharma <rkumarsharma845@gmail.com>
2022-09-14 11:07:28 +0530

    Adding sum

--- src/test/java/gitPractice/Sum.java
+++ src/test/java/gitPractice/Sum.java
@@ -5,7 +5,7 @@ public class Sum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=0,b=1;
-		System.out.println ("Addition is" + a+b);
+		System.out.println ("Addition is:" + a+b);
 	}
 
 }
