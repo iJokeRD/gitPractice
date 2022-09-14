6593602a0349eef798a7fab1177667bca544bb98
Rahul Kumar Sharma <rkumarsharma845@gmail.com>
2022-09-14 10:57:58 +0530

    Initial commit

---
+++ src/test/java/gitPractice/Sum.java
@@ -0,0 +1,11 @@
+package gitPractice;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0,b=1;
+		System.out.println ("Addition is" + a+b);
+	}
+
+}
